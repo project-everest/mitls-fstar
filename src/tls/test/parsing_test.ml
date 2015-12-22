750b107ff440d40b07fcce5ba59ab6c914180e13
Jean Karim <jkzinzindohoue@gmail.com>
2015-12-22 13:17:05 +0100

    added traces to test file

--- src/tls/test/parsing_test.ml
+++ src/tls/test/parsing_test.ml
@@ -8,13 +8,16 @@ open TLSConstants
 let pv = ref TLS_1p2
 let kex = ref Kex_DHE
        
-let log_bytes =
-  { bl = ["abcde"];
-    max = 65536;
-    index = 0;
-    length = 5; }
-
-let rec parse_handshake_message bytes =
+(* Traces list *)
+let traces = [
+    "tls-unique-trace-2.bin";
+    "client-auth-trace-1.bin";
+    "client-auth-trace-2.bin";
+    "openssl-google.hex";
+    "tls-unique-trace-1.bin";
+  ]
+    
+let parse_handshake_message bytes =
   if length bytes >= 4 then
     let ht, len, msg = split2 bytes 1 3 in
     let len = int_of_bytes len in
@@ -29,7 +32,8 @@ let rec parse_handshake_message bytes =
       | '\x02' -> 
 	 (match parseServerHello msg with
 	 | Correct(ch) -> print_string "ServerHello OK\n"
-	 | Error(z) -> print_string "Failed to parse server hello\n")
+	 | Error(z) -> print_string "Failed to parse server hello :\n";
+	               print_string (snd z))
       | '\x04' -> 
 	 (match parseSessionTicket msg with
 	 | Correct(ch) -> print_string "SessionTicket OK\n"
@@ -73,10 +77,20 @@ let rec parse_handshake_message bytes =
 	 (match parseFinished msg with
 	 | Correct(ch) -> print_string "FinishedMessage OK\n"
 	 | Error(z) -> print_string "Failed to parse finished message\n")
-      | '\x18' -> print_string "Error: ignored key update message"
-      | _ -> print_string "Error: parsed an unknown handshake type"
+      | '\x18' -> print_string "Error: ignored key update message\n"
+      | _ -> print_string "Error: parsed an unknown handshake type\n"
   else print_string "Error: HS message too small to retrieve handshake type + length from it\n"
 
+let rec parse_handshake bytes =
+  if length bytes >= 4 then
+    let ht, len, data = split2 bytes 1 3 in
+    let len = int_of_bytes len in
+    let (msg, bytes) = split bytes (4+len) in
+    parse_handshake_message msg;
+    parse_handshake bytes
+  else if length bytes = 0 then print_string "Parsed all messages\n"
+  else print_string "Error: parsing failed due to length not being appropriate"
+		    
 let parse_alert_message bytes =
   ()
 
@@ -103,6 +117,16 @@ let rec parse_message bytes =
   else if length bytes = 0 then ()
   else print_string "Error : Found some standalone bytes"
 
+let parse_trace_file file =
+  let bytes = Bytes.create 65536 in
+  let flag = input file bytes 0 65536 in
+  if flag = 0 then print_string "Reached EOF\n"
+  else print_string ("Read " ^ (string_of_int flag) ^ " characters\n");
+  let bytes = { bl = [bytes]; max = Bytes.length bytes; index = 0; length = Bytes.length bytes; } in
+  parse_handshake bytes;
+  print_string "\n"
+		    
 let _ =
-  parse_message log_bytes
+  let handshakes = List.map (fun x -> open_in ("./traces/" ^ x)) traces in 
+  List.iter parse_trace_file handshakes
   
