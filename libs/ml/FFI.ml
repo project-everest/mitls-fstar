9ab0618266f80cf220778f47bad7d4c49db942d3
BarryBo <barrybo@microsoft.com>
2016-07-11 12:17:31 -0700

    Refactor the FFI client code out of TestClient/TestClient13
    Refactor the FFI client code out of TestClient/TestClient13

--- libs/ml/FFI.ml
+++ libs/ml/FFI.ml
@@ -1,6 +1,6 @@
 let wrappedConnect12 config host = 
-    TestClient.ffiConnect12 () () () () () () config host
+    TLSClient.connect () () () () () () config host
 
-let _ = Callback.register "MITLS_FFI_Config" TestClient13.ffiConfig;
+let _ = Callback.register "MITLS_FFI_Config" TLSClient13.configure;
         Callback.register "MITLS_FFI_Connect12" wrappedConnect12;
-        Callback.register "MITLS_FFI_Connect13" TestClient13.ffiConnect13;
+        Callback.register "MITLS_FFI_Connect13" TLSClient13.connect;
