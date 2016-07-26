0d8d31eae20800dec324629469292595725420d0
BarryBo <barrybo@microsoft.com>
2016-07-26 11:58:30 -0700

    Move FFIRegister.ml to tests directory
    The src directory isn't a good place to store a .ml file as the clean
    target removes them, and they are all excluded via .gitignore.

---
+++ src/tls/test/FFIRegister.ml
@@ -0,0 +1,5 @@
+let _ = Callback.register "MITLS_FFI_Config" TestAPI.ffiConfig;
+        Callback.register "MITLS_FFI_Connect"  TestAPI.ffiConnect;
+        Callback.register "MITLS_FFI_Send" TestAPI.ffiSend;
+        Callback.register "MITLS_FFI_Recv" TestAPI.ffiRecv;
+        
