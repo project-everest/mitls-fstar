cc02da5f76b8740d839a816378f45ab0b8ceb812
Jared Roesch <roeschinc@gmail.com>
2017-08-07 13:26:26 -0700

    Progress towards compiling all of miTLS

--- src/tls/Extracted.mk
+++ src/tls/Extracted.mk
@@ -115,12 +115,12 @@ CEXE_PATH=LD_LIBRARY_PATH=./
 endif
 endif
 
-KRML_FILES = Flags.fst TLSError.fst TLSConstants.fst
+KRML_FILES = $(wildcard *.fst) # Flags.fst TLSError.fst TLSConstants.fst
 C_DRIVER = $(KRML_DIR)/main.c
 HACKS_HEADER = '"hacks.h"'
 
 kremlin:
-	FSTAR_HOME=$(FSTAR_HOME) $(KRML) $(KRML_INCLUDE_PATHS) $(KRML_FILES) $(C_DRIVER) -I concrete-flags -fnoanonymous-unions -add-include $(HACKS_HEADER) -tmpdir $(KRML_DIR)
+	FSTAR_HOME=$(FSTAR_HOME) $(KRML) $(KRML_INCLUDE_PATHS) $(KRML_FILES) $(C_DRIVER) -I concrete-flags -add-include $(HACKS_HEADER) -fnoanonymous-unions -tmpdir $(KRML_DIR) -warn-error +11
 
 test: test.out mitls.exe cmitls.exe
 	# Unit tests from test/test_main.ml
