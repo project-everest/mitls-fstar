f984ae4847624bbf61e6ae1ec8b9ac2ccf75cf17
Jared Roesch <roeschinc@gmail.com>
2017-08-22 10:25:30 -0700

    Change Makefile to use wildcard for Kremlin

--- src/tls/Extracted.mk
+++ src/tls/Extracted.mk
@@ -118,17 +118,6 @@ endif
 endif
 
 KRML_FILES = $(wildcard *.fst)
-# TLSError.fst Parse.fst Hashing.Spec.fst TLSConstants.fst MonotoneMap.fst Nonce.fst \
-# RSAKey.fst Curve25519.fst ECGroup.fst DHGroup.fst CommonDH.fst PMS.fst PSK.fst \
-# Extensions.fst Cert.fst TLSInfo.fst AEADOpenssl.fst Crypto.Indexing.fst AEADProvider.fst \
-# Range.fst DataStream.fst Alert.fst Content.fst StreamPlain.fst StreamAE.fst StatefulPlain.fst \
-# LHAEPlain.fst AEAD_GCM.fst StatefulLHAE.fst StAE.fst HandshakeMessages.fst Hashing.OpenSSL.fst \
-# Hashing.fst Signature.fst Ticket.fst Negotiation.fst HMAC.fst HKDF.fst HMAC.UFCMA.fst \
-# TLSPRF.fst KeySchedule.fst Epochs.fst Hashing.CRF.fst HandshakeLog.fst \
-# Handshake.fst Transport.fst Record.fst Connection.fst StreamDeltas.fst \
-# TLS.fst FFI.fst # QUIC.fst
-
-#$(wildcard *.fst) # Flags.fst TLSError.fst TLSConstants.fst
 C_DRIVER = $(KRML_DIR)/main.c
 HACKS_HEADER = '"hacks.h"'
 
