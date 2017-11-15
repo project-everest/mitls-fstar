3de238d2029473254ca0f16b1e43b25bbd893823
Jonathan Protzenko <protz@microsoft.com>
2017-11-15 12:25:29 -0800

    Restore linking of TLSConstants

--- src/tls/extract/src/support.c
+++ src/tls/extract/src/support.c
@@ -2,6 +2,7 @@
 // declared via kremlin as extern
 
 #include "kremlib.h"
+#include "kremstr.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -10,4 +11,10 @@
 extern bool FStar_IO_debug_print_string(const char *msg) {
   printf("%s\n", msg);
   return true;
+}
+
+extern Prims_string Platform_Error_perror(Prims_string x0, Prims_int x1, Prims_string x2) {
+  char *buf = malloc(512);
+  snprintf(buf, 512, "perror: %s %d %s", x0, x1, x2);
+  return buf;
 }
