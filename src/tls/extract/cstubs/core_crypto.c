a9bdc0afc3d3d41cda8ce24c81445316a9b832e2
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-22 13:38:21 -0800

    More C boilerplate

--- src/tls/extract/cstubs/core_crypto.c
+++ src/tls/extract/cstubs/core_crypto.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <openssl/conf.h>
+#include <openssl/err.h>
+#include <openssl/bn.h>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/dsa.h>
+#include <openssl/dh.h>
+#include <openssl/pem.h>
+#include <openssl/ec.h>
+#include <openssl/objects.h>
+#include <openssl/obj_mac.h>
+
 #define FAIL_IF(test, msg)                                                     \
   do {                                                                         \
     if (test)                                                                  \
@@ -23,24 +37,77 @@
 
 FStar_Bytes_bytes CoreCrypto_dh_agreement(CoreCrypto_dh_key x0,
                                           FStar_Bytes_bytes x1) {
-  /* DH *dh = DH_new();
-  BIGNUM *p = BN_bin2bn(x0.dh_params.dh_p.data, x0.dh_params.dh_p.length, NULL);
-  BIGNUM *q = BN_bin2bn(x0.dh_params.dh_q.data, x0.dh_params.dh_q.length, NULL);
-  BIGNUM *pub = BN_bin2bn(x0.dh_public.data, x0.dh_public.length, NULL);
-  FAIL_IF("OpenSSL allocation failure p/q/pub", p == NULL || q == NULL || pub ==
-  NULL); BIGNUM *prv = NULL; if (x0.dh_private.tag ==
-  FStar_Pervasives_Native_Some) { prv =
-  BN_bin2bn(x0.dh_private.val.case_Some.data,
-  x0.dh_private.val.case_Some.length, NULL); FAIL_IF("OpenSSL allocation failure
-  prv", prv == NULL);
+  DH *dh = DH_new();
+  FAIL_IF(dh == NULL, "OpenSSL allocation failure dh");
+  BIGNUM *p = BN_bin2bn((uint8_t *) x0.dh_params.dh_p.data, x0.dh_params.dh_p.length, NULL);
+  BIGNUM *g = BN_bin2bn((uint8_t *) x0.dh_params.dh_g.data, x0.dh_params.dh_g.length, NULL);
+  BIGNUM *pub = BN_bin2bn((uint8_t *) x0.dh_public.data, x0.dh_public.length, NULL);
+  FAIL_IF(p == NULL || g == NULL || pub == NULL,
+          "OpenSSL allocation failure p/q/pub");
+  BIGNUM *prv = NULL;
+  if (x0.dh_private.tag == FStar_Pervasives_Native_Some) {
+    prv = BN_bin2bn((uint8_t *) x0.dh_private.val.case_Some.v.data,
+                    x0.dh_private.val.case_Some.v.length, NULL);
+    FAIL_IF(prv == NULL, "OpenSSL allocation failure prv");
   }
   DH_set0_pqg(dh, p, NULL, g);
-  DH_set0_key(dh, pub, prv); */
-  TODO(FStar_Bytes_bytes);
+  DH_set0_key(dh, pub, prv);
+
+  uint32_t len = DH_size(dh);
+  char *out = malloc(len);
+
+  FAIL_IF(DH_compute_key((uint8_t *) out, pub, dh) < 0, "OpenSSL failure DH_compute_key");
+
+  if (prv != NULL)
+    BN_free(prv);
+  BN_free(pub);
+  BN_free(g);
+  BN_free(p);
+  DH_free(dh);
+
+  FStar_Bytes_bytes ret = {
+    .length = len,
+    .data = out
+  };
+  return ret;
+}
+
+static inline
+FStar_Bytes_bytes bytes_of_bn(const BIGNUM *bn) {
+  size_t len = BN_num_bytes(bn);
+  char *data = malloc(len);
+  BN_bn2bin(bn, (uint8_t *) data);
+  FStar_Bytes_bytes ret = {
+    .length = len,
+    .data = data
+  };
+  return ret;
 }
 
 CoreCrypto_dh_key CoreCrypto_dh_gen_key(CoreCrypto_dh_params x0) {
-  TODO(CoreCrypto_dh_key);
+  DH *dh = DH_new();
+  FAIL_IF(dh == NULL, "OpenSSL allocation failure dh");
+  BIGNUM *p = BN_bin2bn((uint8_t *) x0.dh_p.data, x0.dh_p.length, NULL);
+  BIGNUM *g = BN_bin2bn((uint8_t *) x0.dh_g.data, x0.dh_g.length, NULL);
+  FAIL_IF(p == NULL || g == NULL, "OpenSSL allocation failure p/g");
+  DH_set0_pqg(dh, p, NULL, g);
+  FAIL_IF(DH_generate_key(dh) == 0, "OpenSSL failure DH_generate_key");
+
+  const BIGNUM *pub, *prv;
+  DH_get0_key(dh, &pub, &prv);
+
+  size_t p_byte_len = BN_num_bytes(pub);
+
+  CoreCrypto_dh_key ret = {
+    .dh_params = x0,
+    .dh_public = bytes_of_bn(pub),
+    .dh_private = {
+      .tag = FStar_Pervasives_Native_Some,
+      .val = { .case_Some = { .v = bytes_of_bn(prv) } }
+    }
+  };
+  return ret;
+    
 }
 
 FStar_Bytes_bytes CoreCrypto_ecdh_agreement(CoreCrypto_ec_key x0,
@@ -63,6 +130,7 @@ CoreCrypto_get_key_from_cert(FStar_Bytes_bytes x0) {
   TODO(FStar_Pervasives_Native_option__CoreCrypto_key);
 }
 
+static inline
 Crypto_HMAC_alg hacl_alg_of_corecrypto_alg(CoreCrypto_hash_alg h) {
   switch (h) {
   case CoreCrypto_SHA256:
@@ -79,15 +147,13 @@ Crypto_HMAC_alg hacl_alg_of_corecrypto_alg(CoreCrypto_hash_alg h) {
 FStar_Bytes_bytes CoreCrypto_hash(CoreCrypto_hash_alg x0,
                                   FStar_Bytes_bytes x1) {
   Crypto_HMAC_alg a = hacl_alg_of_corecrypto_alg(x0);
-  FAIL_IF(a == -1, "CoreCrypto_hash implemented using HACL*, unsupported algorithm");
+  FAIL_IF(a == -1,
+          "CoreCrypto_hash implemented using HACL*, unsupported algorithm");
   uint32_t len = Crypto_HMAC_hash_size(a);
   char *out = malloc(len);
-  Crypto_HMAC_agile_hash(a, (uint8_t *) out, (uint8_t *) x1.data, x1.length);
+  Crypto_HMAC_agile_hash(a, (uint8_t *)out, (uint8_t *)x1.data, x1.length);
 
-  FStar_Bytes_bytes ret = {
-    .length = len,
-    .data = out
-  };
+  FStar_Bytes_bytes ret = {.length = len, .data = out};
   return ret;
 }
 
