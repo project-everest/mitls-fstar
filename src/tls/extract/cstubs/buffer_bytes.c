932cdc3bd1ab51ed335ba890ea6fe64e6a29544d
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-02-09 14:06:29 +0000

    Implement BufferBytes_from_bytes in C

--- src/tls/extract/cstubs/buffer_bytes.c
+++ src/tls/extract/cstubs/buffer_bytes.c
@@ -7,6 +7,7 @@ FStar_Bytes_bytes BufferBytes_to_bytes(Prims_nat l, uint8_t *buf) {
   if (buf == NULL || l == 0)
     return FStar_Bytes_empty_bytes;
   char *data = KRML_HOST_MALLOC(l);
+  CHECK(data);
   memcpy(data, buf, l);
   FStar_Bytes_bytes r = {.length = l, .data = data};
   return r;
@@ -17,4 +18,11 @@ void BufferBytes_store_bytes(Prims_nat len, uint8_t *buf, Prims_nat i,
   assert(i <= len);
   if (b.length > 0 && i < len)
     memcpy(buf + i, b.data + i, len - i);
+}
+
+uint8_t *BufferBytes_from_bytes(FStar_Bytes_bytes b) {
+  uint8_t *buf = KRML_HOST_MALLOC(b.length);
+  CHECK(buf);
+  BufferBytes_store_bytes(b.length, buf, 0, b);
+  return buf;
 }
