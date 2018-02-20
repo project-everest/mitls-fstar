c896a67775abcb4b30a4bc8c982b143d70186e0a
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-02-20 21:58:12 +0000

    Line endings?

--- scripts/vsbin.sh
+++ scripts/vsbin.sh
@@ -6,11 +6,11 @@
 # https://docs.microsoft.com/en-us/visualstudio/extensibility/locating-visual-studio
 # https://blogs.msdn.microsoft.com/vcblog/2017/03/06/finding-the-visual-c-compiler-tools-in-visual-studio-2017/
 if
-  VSWHERE_WINDOWS="$(cmd.exe /C 'echo %ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe' | sed 's!\r!!g')" &&
-  VSWHERE=$(cygpath -u "$VSWHERE_WINDOWS") &&
-  VS_HOME=$("$VSWHERE" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -format value -property InstallationPath | sed 's!\r!!g') &&
-  VC_VERSION=$(cat "$VS_HOME"/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt | sed 's!\r!!g') &&
-  [[ -n "$VS_HOME" ]]
+    VSWHERE_WINDOWS="$(cmd.exe /C 'echo %ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe' | sed 's!\r!!g')" &&
+    VSWHERE=$(cygpath -u "$VSWHERE_WINDOWS") &&
+    VS_HOME=$("$VSWHERE" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -format value -property InstallationPath | sed 's!\r!!g') &&
+    VC_VERSION=$(cat "$VS_HOME"/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt | sed 's!\r!!g') &&
+    [[ -n "$VS_HOME" ]]
 then
   VS_BIN_DOSPATH="$VS_HOME"/VC/Tools/MSVC/"$VC_VERSION"/bin/Hostx64/x64
 else
